cdc358c6a98bff8d7a17aaaeb8ed697d0bb24a45
Johnny Wannamaker <j.wannamaker19@gmail.com>
2024-05-21 14:58:06 -0700

    Basic main window setup

--- src/warpool.py
+++ src/warpool.py
@@ -1,7 +1,23 @@
 import pyglet
+from pyglet import gl
+from pyglet.window import key, mouse
 
-window = pyglet.window.Window()
-window.set_fullscreen(True)
+
+# Allows images to load with their alpha values blended? 
+# I definitely need to learn how OpenGL works. (Eventually)
+gl.glEnable(gl.GL_BLEND)
+gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+
+main_window = pyglet.window.Window()
+main_window.set_fullscreen(True)
+
+main_batch = pyglet.graphics.Batch()
+
+
+@main_window.event
+def on_draw():
+    main_window.clear()
+    main_batch.draw()
 
 if __name__ == '__main__':
     pyglet.app.run()
