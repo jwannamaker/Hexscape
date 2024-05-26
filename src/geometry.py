c683b1c60d64fe95a62d7bd7a0f668e7079ce287
Johnny Wannamaker <j.wannamaker19@gmail.com>
2024-05-26 12:09:03 -0700

    Renamed diagonal keys to avoid duplicates w/adjacent

--- src/geometry.py
+++ src/geometry.py
@@ -1,3 +1,11 @@
+"""
+Horizontal distance between two adjacent flat top regular hexagons
+    h = 3/4 * WIDTH = 3/2 * RADIUS
+
+Vertical distance between two adjacent flat top regular hexagons
+    v = HEIGHT = sqrt(3) * RADIUS
+"""
+
 from dataclasses import dataclass, field
 from collections import namedtuple
 import operator
@@ -26,21 +34,6 @@ def z_rotation(point, angle):
                     [0, 0, 1]])
     return rot_z @ point
 
-# def hexagon(center_x, center_y, radius, batch):
-#     """ Reusing code from PolyBounce """
-#     step = 60 # degrees
-#     start = (180 - step) / 2  # flat top orientation
-#     stop = start + 360 + step
-#     angles = np.arange(np.deg2rad(start), np.deg2rad(stop), np.deg2rad(step))
-#     vertices = []
-#     for i, angle in enumerate(angles):
-#         x = radius * np.cos(angle) + center_x
-#         y = radius * np.sin(angle) + center_y
-#         vertices.append([x, y])
-        
-#     hexagon = pyglet.shapes.Polygon(*vertices, batch=batch)
-#     border = pyglet.shapes.MultiLine(*vertices, thickness=5, color=(200, 200, 200), batch=batch)
-#     return border
 @dataclass
 class Hex:
     q: int
@@ -64,13 +57,14 @@ class Hex:
     
 
 class HexOrientation:
-    def __init__(self, radius=64, flat_top=True):
-        self.RADIUS = radius
-        self.WIDTH = 2 * self.RADIUS
-        self.HEIGHT = np.sqrt(3) * self.RADIUS
+    def __init__(self, origin_x: int, origin_y: int, radius: int = 64, flat_top: bool = True):
+        """ (origin_x, origin_y): screen coordinate for center of Hex(0, 0, 0). """
+        self._radius = radius
+        self._width = 2 * self.radius
+        self._height = np.sqrt(3) * self._radius
 
-        self.HORIZONTAL_DISTANCE = 3 / 4 * self.WIDTH
-        self.VERTICAL_DISTANCE = self.HEIGHT
+        self.i = 3 / 2 * self._radius
+        self.j = np.sqrt(3) / 2 * self._radius
 
         if flat_top:
             step = 60 # degrees
@@ -79,7 +73,12 @@ class HexOrientation:
         self.CORNER_ANGLES = np.arange(start, stop, step)
 
     def corners(self, hex):
-        return
+        corners = []
+        for angle in self.CORNER_ANGLES:
+            x = self._radius * np.cos(np.deg2rad(angle)) + hex()
+            y = self._radius * np.sin(np.deg2rad(angle)) + hex()
+            corners.append((x, y))
+        return corners
 
     ADJACENT_DIRECTION = {
         'UP_RIGHT':   Hex(+1, -1,  0), 
@@ -91,15 +90,16 @@ class HexOrientation:
     }
     
     DIAGONAL_DIRECTION = {
-        'RIGHT':      Hex(+2, -1, -1),
-        'UP_RIGHT':   Hex(+1, -2, +1),
-        'UP_LEFT':    Hex(-1, -1, +2),
-        'LEFT':       Hex(-2, +1, +1),
-        'DOWN_LEFT':  Hex(-1, +2, -1),
-        'DOWN_RIGHT': Hex(+1, +1, -2)
+        'RIGHT':           Hex(+2, -1, -1),
+        'UP_UP_RIGHT':     Hex(+1, -2, +1),
+        'UP_UP_LEFT':      Hex(-1, -1, +2),
+        'LEFT':            Hex(-2, +1, +1),
+        'DOWN_DOWN_LEFT':  Hex(-1, +2, -1),
+        'DOWN_DOWN_RIGHT': Hex(+1, +1, -2)
     }
     
     def neighbor(self, hex: Hex, direction: str):
+        """ Return the hex coordinate for the neighbor of hex in direction """
         if direction in HexOrientation.ADJACENT_DIRECTION:
             return hex + HexOrientation.ADJACENT_DIRECTION(direction)
         if direction in HexOrientation.DIAGONAL_DIRECTION:
@@ -111,7 +111,10 @@ class HexLayout:
         self.HexTiles = {}
         
     def nearby(self, hex: Hex, search_distance: int):
-        """ Returns all nearby tiles within the given search_distance. """
+        """ Returns a list of hex coordinates within search_distance to the given 
+        hex coordinate.
+        Note: The coordinates WILL need to be validated by the map.
+        """
         results = []
         for q in range(-search_distance, search_distance, 1):
             # Find only the revelvant values for r to iterate over, considering
@@ -123,26 +126,11 @@ class HexLayout:
                 results.append(hex + Hex(q, r, s))
         return results
             
-"""
-Horizontal distance between two adjacent flat top regular hexagons
-    h = 3/4 * WIDTH = 3/2 * RADIUS
 
-Vertical distance between two adjacent flat top regular hexagons
-    v = HEIGHT = sqrt(3) * radius
-"""
-
-    
-    
-@dataclass
-class HexTile: 
-    hex_coord: Hex
-    center: tuple[int, int] = field(kw_only=True)
-    
     
 
 if __name__ == '__main__':
-    bestagon = Hex(0, 0)
+    orientation = HexOrientation()
+    bestagon = Hex(0, 0, 0)
+    orientation.corners(bestagon)
     print(bestagon)
-    print(bestagon.corners(150, 150))
-    print(bestagon.up())
-    print(bestagon.up().corners())
